c482705a8f29d40ae18379d64348f2d58e9d6c60
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-21 19:47:46 +0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(fullArray) {
+    let itemsToRemove = Array.from(arguments);
+    itemsToRemove.shift();
 
+    let newArray = [];
+
+    for(let fullArrayItem of fullArray){
+        if(!itemsToRemove.includes(fullArrayItem)) newArray.push(fullArrayItem);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
