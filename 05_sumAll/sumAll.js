e2c8712ca87b877eed7017d617bed682ea67699a
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-21 20:27:44 +0800

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start, end;
 
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number' || num1 < 0 || num2 < 0) return 'ERROR';
+
+    if(num1 <= num2){
+       start = num1;
+       end = num2;
+    } else {
+        start = num2;
+        end = num1;
+    }
+
+    for(start; start <=end; start++){
+        sum += start;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
