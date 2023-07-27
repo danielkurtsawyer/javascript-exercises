258e2f95737da94e92ccaa4c74a14f61dd888685
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-27 17:38:09 +0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const TODAYS_YEAR = new Date().getFullYear();
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        return personAge > oldestAge ? person : oldest;
+    });
 };
+
+const getAge = function(birthYear, deathYear){
+    if(!deathYear){
+        deathYear = TODAYS_YEAR;
+    }
+    return deathYear - birthYear;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
