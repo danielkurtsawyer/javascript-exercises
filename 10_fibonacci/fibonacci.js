4813f8f02aa96dc4f54d7045eb0c3f14c21d0608
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-27 17:01:21 +0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthMember) {
+    if(nthMember < 0) return 'OOPS';
+    else if (+nthMember === 0) return 0;
+    else if (+nthMember === 1 || +nthMember === 2) return 1;
+    else return fibonacci(+nthMember-1) + fibonacci(+nthMember-2);
 };
 
 // Do not edit below this line
